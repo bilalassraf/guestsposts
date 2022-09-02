720bc14d2811b1a8af483bd1b31e9d1917108df4
Usman Ibrahim <usmanibrahim3850@gmail.com>
2022-09-02 19:19:46 +0500

    add new ab

--- resources/views/filter.blade.php
+++ resources/views/filter.blade.php
@@ -12,32 +12,12 @@
     <div class="col-md-10 offset-2">
         <div class="collapse" id="advanceFilter">
             <div id="advance-filter">
-                <div class="card p-5">
+                <div class="card">
                     <div class="card-body">
                         <form action="{{ route('filter.route') }}" method="POST">
                             @csrf
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <label>Categories</label><br>
-                                    {{-- <input type="text" name="category" class="mb-2" style="width:100%"> --}}
-                                        <select name="category" id="" class="form-control mb-2">
-                                        @foreach ($categories as $category)
-                                        <option value="{{ $category->id }}">{{ $category->category }}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                                <div class="col-md-6">
-                                    <label for="cars">Status:</label><br>
-                                    <select name="status" class="form-control mb-2">
-                                        <option value="Approved">Approved</option>
-                                        <option value="Pending">Pending</option>
-                                        <option value="Rejected">Rejected</option>
-                                        <option value="Deleted">Deleted</option>
-                                    </select>
-                                </div>
-                            </div>
                             <div class="row mt-2">
-                               <div class="col-md-9">
+                               <div class="col-md-8">
                                     <div class="col-lg-12 col-md-6">
                                         <div>
                                             <div class="row">
@@ -52,7 +32,7 @@
                                                         <input type="number" min="1" max="{{ $filter->authority }}" name="domain_upper" class="domain_input range_input mt-2 form-control" value="1" id="domain_upper" oninput="document.getElementById('authority_upper_bar').value = this.value">
                                                     </div>
                                                 </div>
-                                                <div class="col-lg-7 margintop">
+                                                <div class="col-lg-6 margintop">
                                                     <span class="multi-range d-block">
                                                         <input type="range" min="1" max="{{ $filter->authority }}" oninput="document.getElementById('domain_upper').value = this.value" class="range_input" name="domain_upper"  value="1" data-id="authority-upper" id="authority_upper_bar">
                                                         <input type="range" min="1" max="{{ $filter->authority }}" oninput="document.getElementById('domain_lower').value = this.value" class="range_input" name="domain_lower"  value="{{ $filter->authority }}" data-id="authority-lower" id="authority_lower_bar">
@@ -81,7 +61,7 @@
                                                         <input type="number" min="1" max="{{ $filter->raitings  }}" name="raitings_upper" class="domainRate_input range_input mt-2 form-control" value="1" id="raitings_upper" oninput="document.getElementById('raitings_upper_bar').value = this.value">
                                                     </div>
                                                 </div>
-                                                <div class="col-lg-7 margintop">
+                                                <div class="col-lg-6 margintop">
                                                     <span class="multi-range d-block">
                                                         <input type="range" min="1" max="{{ $filter->raitings }}" class="range_input" name="raitings_upper"  value="1"  oninput="document.getElementById('raitings_upper').value = this.value" id="raitings_upper_bar">
                                                         <input type="range" min="1" max="{{ $filter->raitings }}" class="range_input" name="raitings_lower"  value="{{ $filter->authority }}" oninput="document.getElementById('raitings_lower').value = this.value" id="raitings_lower_bar">
@@ -111,7 +91,7 @@
                                                         <input type="number" min="1" max="{{ $filter->spam_score  }}" name="span_upper" class="spamScore_input range_input mt-2 form-control" value="1" id="span_upper" oninput="document.getElementById('span_upper_bar').value = this.value">
                                                     </div>
                                                 </div>
-                                                <div class="col-lg-7 margintop">
+                                                <div class="col-lg-6 margintop">
                                                     <span class="multi-range d-block">
                                                         <input type="range" min="1" max="{{ $filter->spam_score }}" class="range_input" name="span_upper"  value="1" oninput="document.getElementById('span_upper').value = this.value" id="span_upper_bar">
                                                         <input type="range" min="1" max="{{ $filter->spam_score }}" class="range_input" name="span_lower"  value="{{ $filter->spam_score }}" oninput="document.getElementById('span_lower').value = this.value" id="span_lower_bar">
@@ -140,7 +120,7 @@
                                                         <input type="number" min="1" max="{{ $filter->ahrefs_traffic  }}" name="traffic_upper" class="orgTraffic_input range_input mt-2 form-control" value="1" id="traffic-upper" oninput="document.getElementById('traffic_upper_bar').value = this.value">
                                                     </div>
                                                 </div>
-                                                <div class="col-lg-7 margintop">
+                                                <div class="col-lg-6 margintop">
                                                     <span class="multi-range d-block">
                                                         <input type="range" min="1" max="{{ $filter->ahrefs_traffic }}" class="range_input" name="traffic_upper"  value="1" oninput="document.getElementById('traffic-upper').value = this.value" id="traffic_upper_bar">
                                                         <input type="range" min="1" max="{{ $filter->ahrefs_traffic }}" class="range_input" name="traffic_lower"  value="{{ $filter->ahrefs_traffic }}" oninput="document.getElementById('traffic-lower').value = this.value" id="traffic_lower_bar">
@@ -169,7 +149,7 @@
                                                         <input type="number" min="1" max="{{ $filter->company_price  }}" name="company_upper" class="company_input range_input mt-2 form-control" value="1" id="company_upper" oninput="document.getElementById('company_upper_bar').value = this.value">
                                                     </div>
                                                 </div>
-                                                <div class="col-lg-7 margintop">
+                                                <div class="col-lg-6 margintop">
                                                     <span class="multi-range d-block">
                                                         <input type="range" min="1" max="{{ $filter->company_price }}" class="range_input" name="company_upper"  value="1" oninput="document.getElementById('company_upper').value = this.value" id="company_upper_bar">
                                                         <input type="range" min="1" max="{{ $filter->company_price }}" class="range_input" name="company_lower"  value="{{ $filter->company_price }}" oninput="document.getElementById('company_lower').value = this.value" id="company_lower_bar">
@@ -190,7 +170,7 @@
                                             <div class="row">
                                                 <div class="col-lg-1">
                                                     <span class="multi-range-heading d-block">
-                                                        SEM
+                                                        ST
                                                     <span class="multi-range d-block">    
                                                 </div>
                                                 <div class="col-lg-2">
@@ -198,7 +178,7 @@
                                                         <input type="number" min="1" max="{{ $filter->sem_traffic  }}" name="organic_upper" class="semTraffic_input range_input mt-2 form-control" value="1" id="organic_upper" oninput="document.getElementById('organic_upper_bar').value = this.value">
                                                     </div>
                                                 </div>
-                                                <div class="col-lg-7 margintop">
+                                                <div class="col-lg-6 margintop">
                                                     <span class="multi-range d-block">
                                                         <input type="range" min="1" max="{{ $filter->sem_traffic }}" class="range_input" name="organic_upper"  value="1" oninput="document.getElementById('organic_upper').value = this.value" id="organic_upper_bar">
                                                         <input type="range" min="1" max="{{ $filter->sem_traffic }}" class="range_input" name="organic_lower"  value="{{ $filter->sem_traffic }}" oninput="document.getElementById('organic_lower').value = this.value" id="organic_lower_bar">
@@ -227,7 +207,7 @@
                                                         <input type="number" min="1" max="{{ $filter->web_price  }}" name="web_upper" class="webPrice_input range_input mt-2 form-control" value="1" id="web_upper"  oninput="document.getElementById('web_upper_bar').value = this.value">
                                                     </div>
                                                 </div>
-                                                <div class="col-lg-7 margintop">
+                                                <div class="col-lg-6 margintop">
                                                     <span class="multi-range d-block">
                                                         <input type="range" min="1" max="{{ $filter->web_price }}" class="range_input" name="web_upper"  value="1" oninput="document.getElementById('web_upper').value = this.value" id="web_upper_bar">
                                                         <input type="range" min="1" max="{{ $filter->web_price }}" class="range_input" name="web_lower"  value="{{ $filter->web_price }}" oninput="document.getElementById('web_lower').value = this.value" id="web_lower_bar">
@@ -243,18 +223,34 @@
                                         </div>
                                     </div>
                                </div>
-                            </div>
-                            
-
-                            <div class="row mt-2">
-                                <div class="col-md-6" style="margin-top: 30px;">
-                                    <label for="cars">Date Of the last Update:</label><br>
-                                    <div class="input-group">
-                                        <input type="date" class="form-control" name="to">
-                                        <div class="input-group-append">
-                                            <span class="input-group-text"><i class="fa fa-ellipsis-h"></i></span>
+                               <div class="col-md-4">
+                                    <div class="col-md-12">
+                                        <label>Categories</label><br>
+                                        {{-- <input type="text" name="category" class="mb-2" style="width:100%"> --}}
+                                            <select name="category" id="" class="form-control mb-2">
+                                            @foreach ($categories as $category)
+                                            <option value="{{ $category->id }}">{{ $category->category }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <label for="cars">Status:</label><br>
+                                        <select name="status" class="form-control mb-2">
+                                            <option value="Approved">Approved</option>
+                                            <option value="Pending">Pending</option>
+                                            <option value="Rejected">Rejected</option>
+                                            <option value="Deleted">Deleted</option>
+                                        </select>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <label for="cars">Date Of the last Update:</label><br>
+                                        <div class="input-group">
+                                            <input type="date" class="form-control" name="to">
+                                            <div class="input-group-append">
+                                                <span class="input-group-text"><i class="fa fa-ellipsis-h"></i></span>
+                                            </div>
+                                            <input type="date" class="form-control datepicker hasDatepicker" name="from">
                                         </div>
-                                        <input type="date" class="form-control datepicker hasDatepicker" name="from">
                                     </div>
                                 </div>
                             </div>
