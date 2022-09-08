a7f2d7d77d90d364b2035b2d1e48f74e83f37941
Usman Ibrahim <usmanibrahim3850@gmail.com>
2022-09-08 13:30:09 +0500

    add new 3

--- resources/views/modals/update-request.blade.php
+++ resources/views/modals/update-request.blade.php
@@ -76,7 +76,7 @@
                             <div class="col-md-6">
                                 <div class="mb-3">
                                     <label for="domainauthority">Domain Authority(Moz)</label>
-                                    <input type="text" value="{{ $request->domain_authority }}">
+                                    @dd($request->all())
                                     <input class="form-control"  type="text"
                                         placeholder="Domain Authority" name="domain_authority" required
                                         value="{{ $request->domain_authority }}" id="domainauthoritys">
